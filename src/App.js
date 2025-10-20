968a500329bb0266b410855b1fb58e1cce940612
tazegullu <atazegullu@gmail.com>
2025-10-21 01:14:11 +0300

    Environment Variables

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>{process.env.REACT_APP_API_URL}</p>
       <img src="/logo192.png" alt=""/>
       <img src={logo} alt=""/>
     </div>
