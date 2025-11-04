70a4963e56904fce524af6fa43f9da00288441c7
tazegullu <atazegullu@gmail.com>
2025-11-05 00:48:13 +0300

    React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import styles from './App.module.css'
 import { Title } from './Components';
+import Bootstrap from './Bootstrap';
 
 
 function App() {
@@ -9,7 +10,22 @@ function App() {
       <Title>{process.env.NODE_ENV}</Title>
       <Title theme="dark">{process.env.NODE_ENV}</Title>
       <p>{process.env.REACT_APP_API_URL}</p>
+
+      <p>Logo from public</p>
       <img src="/logo192.png" alt=""/>
+
+      <p>React Bootstrap</p>
+      <Bootstrap/>
+
+
+
+      
+
+
+
+
+
+      <p>Logo from ana dizin</p>
       <img src={logo} alt=""/>
     </div>
   );
---
+++ src/Bootstrap.js
@@ -0,0 +1,25 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Button } from "react-bootstrap"
+//import Button from "react-bootstrap/Button"
+
+
+function Bootstrap(){
+    return (
+        <div>
+            <h3>Bootstrap Components</h3>
+            <Button>Bootstrap Buton</Button>
+            <Button variant='success'>Bootstrap Buton</Button>
+        </div>
+    )
+}
+export default Bootstrap
+
+
+
+
+/*Alternatif
+export default function Bootstrap(){
+
+}*/
